78661d2f792ef0c7346eab10d1dae337b935ce99
NoeFaucher <faucher.noe7@gmail.com>
2023-07-22 15:36:35 +0200

    new start

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,3 @@
-use std::io;
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow,DrawingArea,Button};
-use cairo_rs as cairo;
-use cairo::{Context,ImageSurface,Format};
-use std::f64::consts::PI;
-
-
-mod particule;
-use crate::particule::{Particule,Color};
-
-
-
-mod config_constant;
-
-fn main() {
-    
-    let application = Application::builder()
-        .application_id("com.example.FirstGtkApp")
-        .build();
-
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First GTK Program")
-            .default_width(350)
-            .default_height(350)
-            .build();
-        
-        let da = DrawingArea::new();
-
-        let surface = ImageSurface::create(Format::ARgb32,350,350).unwrap();
-        let cr = Context::new(&surface).unwrap();
-
-        // context.save().expect("unable to save");
-        // //context.translate(0_f64, 0_f64);
-        // context.scale(350_f64 / 2.0, 350_f64 / 2.0);
-        // context.arc(0.0, 0.0, 1.0, 0.0, 2_f64 * PI);
-        // context.restore().expect("unable to restore");
-        // context.save();
-        // context.arc(1.0, 1.0, 30.0, 0.0, 2.0 * PI); // full circle
-        // context.set_source_rgb(255.0, 255.0, 255.0);    // partially translucent
-        // context.fill_preserve();
-        // context.restore();  // back to opaque black
-        // context.stroke();
-
-        // context.save();
-
-        // cr.set_source_rgb(0.0, 0.0, 0.0);
-        // cr.set_line_width(10.0);
-
-        // cr.move_to(0.0, 0.0);
-        // cr.line_to(300.0, 300.0);
-
-        // cr.stroke().expect("fail to stroke");
-
-        let f =150;
-
-        let width = surface.width() as f64;
-        let height = surface.height() as f64;
-        let dx = f as f64 * 1.0;
-
-        cr.set_source_rgb(1.0, 1.0, 1.0);
-        cr.paint();
-
-        let cx = width / 2.0;
-        let cy = height / 2.0;
-        let r = cy;
-        let cstart = -0.5 * PI;
-        let cend = cstart + 2.0 * PI * ((f + 1) as f64) / 300.0;
-        cr.move_to(cx, cy);
-        cr.line_to(cx, 0.0);
-        cr.arc(cx, cy, r, cstart, cend);
-        cr.line_to(cx, cy);
-        cr.set_source_rgba(0.0, 0.5, 0.0, 0.2);
-        cr.fill();
-
-        cr.select_font_face(
-            "sans-serif",
-            cairo::FontSlant::Normal,
-            cairo::FontWeight::Normal,
-        );
-        cr.set_font_size(70.0);
-        cr.move_to(600.0 - dx, 100.0);
-        cr.set_source_rgb(0.0, 0.0, 1.0);
-        cr.show_text("Hello, world! 1234567890");
-        cr.fill();
-
-        da.draw(&cr);
-       
-        window.add(&da);
-
-        window.show_all();
-    });
-
-    application.run();
-
 
     // let test_or = Particule::new_origin(Color::Green, 1.0);
     // let test_rd = Particule::new_rand(100.0,100.0, Color::Blue, 1.0);
@@ -110,5 +13,86 @@ fn main() {
 
     //     println!("ap {} test_or : {:?}",j,tests[0]);
     //     println!("ap {} test_rd : {:?}",j,tests[1]);
-    // }
+
+
+
+use std::f64::consts::PI;
+
+use gtk::gdk::FrameClock;
+use rand::Rng;
+
+use gtk::cairo::Context;
+use gtk::{self, glib};
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, DrawingArea, Box, Orientation};
+
+
+
+fn draw_func(area: &DrawingArea, cr : &Context, width: i32, height: i32) {
+    
+    let context = area.style_context();
+
+    // cr.arc(1.0,1.0,30.0,0.0,2.0*PI) ;
+    let mut rng = rand::thread_rng();
+    let radius = rng.gen_range(20.0..70.0);
+
+    cr.arc(width as f64 / 2.0, height as f64 / 2.0, radius, 0.0, 2.0*PI);
+
+    let color = context.color();
+    cr.set_source_rgba(color.red() as f64, color.green() as f64, color.blue() as f64, color.alpha() as f64);
+
+    let _ = cr.fill();
+    
+}
+
+fn time_handler(area: &DrawingArea, fc: &FrameClock) -> Continue {
+
+    area.queue_draw();
+
+    return Continue(true);
+}
+
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("First GTK Program")
+        .default_width(500)
+        .default_height(500)
+        .build();
+
+    let container_box = Box::new(Orientation::Horizontal,10);
+
+    let drawing_area = DrawingArea::new();
+
+    drawing_area.set_content_width(300);
+    drawing_area.set_content_height(300);
+
+    drawing_area.set_draw_func(draw_func);
+
+    drawing_area.add_tick_callback(time_handler);
+
+    container_box.append(&drawing_area);
+
+    window.set_child(Some(&container_box));
+    
+
+
+    window.show();
+
+}
+
+
+
+
+
+fn main() -> glib::ExitCode {
+    let application = Application::builder()
+        .application_id("com.example.FirstGtkApp")
+        .build();
+
+
+    application.connect_activate(build_ui);
+
+    application.run()
 }
